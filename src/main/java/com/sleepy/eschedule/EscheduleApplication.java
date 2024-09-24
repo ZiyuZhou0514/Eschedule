ab9c9c43ee917a3d81f5d39ce66947d98cdc146e
ZiyuZhou <ziyu_zhou2021@126.com>
2024-09-24 10:41:08 +0800

    first commit

---
+++ src/main/java/com/sleepy/eschedule/EscheduleApplication.java
@@ -0,0 +1,13 @@
+package com.sleepy.eschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sleepy/eschedule/EscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleepy.eschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
